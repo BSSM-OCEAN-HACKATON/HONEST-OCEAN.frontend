687f267239dc1307397ecf12200de6f91cda10da
gamzamandu <galbimandu1126@gmail.com>
2025-12-30 17:59:21 +0900

    feat : 카메라 폼 페이지 추가

---
+++ app/(navless)/camera/form/page.tsx
@@ -0,0 +1,12 @@
+import PriceForm from '@/app/components/shared/PriceForm'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className="flex-col justify-center min-h-[calc(100vh-200px)]">
+      <PriceForm />
+    </div>
+  )
+}
+
+export default page
