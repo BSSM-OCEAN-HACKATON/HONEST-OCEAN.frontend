ca9db674df66bd41909deb8f23c693710efbd84f
gamzamandu <galbimandu1126@gmail.com>
2025-12-30 17:04:02 +0900

    feat : 카메라 페이지 컴포넌트 추가

---
+++ app/(navless)/camera/page.tsx
@@ -0,0 +1,10 @@
+import Button from '@/app/components/shared/Button'
+import React from 'react'
+
+const CameraPage = () => {
+  return (
+    <div><Button text='Take Photo' onClick={() => {}}/></div>
+  )
+}
+
+export default CameraPage
