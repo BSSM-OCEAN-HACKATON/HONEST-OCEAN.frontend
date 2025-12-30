46982528ecb39385b4a27c12b8f58b8fd4adf71a
gamzamandu <galbimandu1126@gmail.com>
2025-12-30 17:59:17 +0900

    feat : QuantityButtons 컴포넌트 추가

---
+++ app/components/shared/QuantityButtons.tsx
@@ -0,0 +1,45 @@
+'use client'
+
+import Image from 'next/image'
+import React from 'react'
+
+interface QuantityButtonsProps {
+  onIncrement?: () => void
+  onDecrement?: () => void
+}
+
+const QuantityButtons = ({ onIncrement, onDecrement }: QuantityButtonsProps) => {
+  return (
+    <div className="flex-row gap-[5px] items-center">
+      <button
+        onClick={onDecrement}
+        className="w-12 h-12"
+        aria-label="감소"
+      >
+        <Image
+          src="/button/minus.svg"
+          alt="minus"
+          width={48}
+          height={51}
+          className="w-full h-full"
+        />
+      </button>
+      <button
+        onClick={onIncrement}
+        className="w-12 h-12"
+        aria-label="증가"
+      >
+        <Image
+          src="/button/plus.svg"
+          alt="plus"
+          width={48}
+          height={51}
+          className="w-full h-full"
+        />
+      </button>
+    </div>
+  )
+}
+
+export default QuantityButtons
+
