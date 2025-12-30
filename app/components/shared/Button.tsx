716892edc2b2e6e81cc53580db29deed2454cd20
gamzamandu <galbimandu1126@gmail.com>
2025-12-30 17:15:29 +0900

    feat : Button 컴포넌트 onClick 핸들러 연결

--- app/components/shared/Button.tsx
+++ app/components/shared/Button.tsx
@@ -7,7 +7,12 @@ interface ButtonProps {
 
 const Button = ({ text, onClick }: ButtonProps) => {
   return (
-    <button className='bg-gray-003 text-white flex-center rounded-md w-full py-2'>{text}</button>
+    <button 
+      className='bg-gray-003 text-white flex-center rounded-md w-full py-2'
+      onClick={onClick}
+    >
+      {text}
+    </button>
   )
 }
 
